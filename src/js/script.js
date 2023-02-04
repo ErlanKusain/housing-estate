76848329dff63c42e53e6500fa5d53ea080675c3
erlankusain <erlankusain@yandex.ru>
2023-02-04 14:56:08 +0600

    Validate forms and smart.php

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,54 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+$(document).ready(function(){ 
+
+  function valideForms(form){
+    $(form).validate({
+      rules: {
+        name: 'required',
+        phone: 'required',
+        email: {
+          required:true,
+          email:true,
+        }
+      },
+      messages: {
+        name: "Пожалуйста,введите своё имя",
+        phone:'Пожалуйста,введите свой номер телефона',
+        email: {
+          required: "Нам нужен ваш адрес для того чтобы связаться с вами",
+          email: "Ваш адрес не соответствует правильному формату"
+        }
+      }
+    });
+  };
+
+  valideForms('#about-form');
+  valideForms('#questions-form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if(!$(this).valid()) {
+      return;
+    };
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      // $('#consultation, #order').fadeOut();
+      // $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+});
+
---
+++ src/mailer/smart.php
@@ -0,0 +1,43 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'erlankusain@yandex.ru';                 // Наш логин
+$mail->Password = 'zxheweesosbkldmk';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('erlankusain@yandex.ru', 'Housing Estate - Жилой комплекс в историческом центре');   // От кого письмо 
+$mail->addAddress('erlan_vip0198@mail.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
